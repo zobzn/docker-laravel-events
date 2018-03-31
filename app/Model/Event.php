df7256aa8568e5222db7130d1066ced5b1fb4e84
zema <zlobzn@gmail.com>
2018-03-31 20:33:48 +0300

    model relations

--- app/Model/Event.php
+++ app/Model/Event.php
@@ -14,4 +14,9 @@ class Event extends Model
     protected $fillable = [
         'title', 'date',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
